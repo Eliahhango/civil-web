40bcc3bf8e4a8414e7f0ef772678167828bf9ea4
Eliahhango <146315119+Eliahhango@users.noreply.github.com>
2026-03-13 00:22:17 +0300

    Remove WordPress PHP endpoint for static Vercel deploy

--- renovex/wp-comments-post.php
+++
@@ -1,1 +0,0 @@
-<html><head><link rel="icon" href="data:;"><meta http-equiv="refresh" content="0;/.well-known/sgcaptcha/?r=%2Frenovex%2Fwp-comments-post.php&y=powu:102.205.250.222:1773348102.480"></meta></head></html>
